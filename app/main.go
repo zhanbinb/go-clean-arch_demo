377bb8c917e6dfaf9143a8289cc6c19f797e6440
zhanbinb <zhangbinbinly@163.com>
2026-07-21 14:55:40 +0800

    Initial commit: Go Clean Architecture demo
    Implements clean architecture pattern with:
    - Domain layer (entities and errors)
    - Repository layer (MySQL implementations + mocks)
    - Service layer (use cases)
    - Delivery layer (REST handlers with Echo)
    - Internal packages (shared utilities)
    - Docker compose setup with MySQL and the application

---
+++ app/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+	"strconv"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/labstack/echo/v4"
+
+	mysqlRepo "github.com/bxcodec/go-clean-arch/internal/repository/mysql"
+
+	"github.com/bxcodec/go-clean-arch/article"
+	"github.com/bxcodec/go-clean-arch/internal/rest"
+	"github.com/bxcodec/go-clean-arch/internal/rest/middleware"
+	"github.com/joho/godotenv"
+)
+
+const (
+	defaultTimeout = 30
+	defaultAddress = ":9090"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func main() {
+	//prepare database
+	dbHost := os.Getenv("DATABASE_HOST")
+	dbPort := os.Getenv("DATABASE_PORT")
+	dbUser := os.Getenv("DATABASE_USER")
+	dbPass := os.Getenv("DATABASE_PASS")
+	dbName := os.Getenv("DATABASE_NAME")
+	connection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPass, dbHost, dbPort, dbName)
+	val := url.Values{}
+	val.Add("parseTime", "1")
+	val.Add("loc", "Asia/Jakarta")
+	dsn := fmt.Sprintf("%s?%s", connection, val.Encode())
+	dbConn, err := sql.Open(`mysql`, dsn)
+	if err != nil {
+		log.Fatal("failed to open connection to database", err)
+	}
+	err = dbConn.Ping()
+	if err != nil {
+		log.Fatal("failed to ping database ", err)
+	}
+
+	defer func() {
+		err := dbConn.Close()
+		if err != nil {
+			log.Fatal("got error when closing the DB connection", err)
+		}
+	}()
+	// prepare echo
+
+	e := echo.New()
+	e.Use(middleware.CORS)
+	timeoutStr := os.Getenv("CONTEXT_TIMEOUT")
+	timeout, err := strconv.Atoi(timeoutStr)
+	if err != nil {
+		log.Println("failed to parse timeout, using default timeout")
+		timeout = defaultTimeout
+	}
+	timeoutContext := time.Duration(timeout) * time.Second
+	e.Use(middleware.SetRequestContextWithTimeout(timeoutContext))
+
+	// Prepare Repository
+	authorRepo := mysqlRepo.NewAuthorRepository(dbConn)
+	articleRepo := mysqlRepo.NewArticleRepository(dbConn)
+
+	// Build service Layer
+	svc := article.NewService(articleRepo, authorRepo)
+	rest.NewArticleHandler(e, svc)
+
+	// Start Server
+	address := os.Getenv("SERVER_ADDRESS")
+	if address == "" {
+		address = defaultAddress
+	}
+	log.Fatal(e.Start(address)) //nolint
+}
